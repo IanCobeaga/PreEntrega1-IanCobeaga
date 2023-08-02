3f7e78fadc55b46e48fa21cc8db5ac6093a628e1
IanCobeaga <iancobeaga96@gmail.com>
2023-08-01 21:07:50 -0300

    Fix - agrega <strictMode>

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import React from "react";
 import CartWidget from "./CartWidget"
 
 const NavBar = ({greeting}) => {
-
+    
     const changeGreeting = (text) => {
         greeting(text)
     }
@@ -12,7 +12,7 @@ const NavBar = ({greeting}) => {
             <div className="container-fluid">
                 <button type="button" className="navbar-brand nav-btn" onClick={() => changeGreeting("HOLA MUNDO!")}>
                     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Escudo_del_Club_Atl%C3%A9tico_San_Lorenzo_de_Almagro.svg/512px-Escudo_del_Club_Atl%C3%A9tico_San_Lorenzo_de_Almagro.svg.png"
-                        height={50} width={50} />
+                        height={42} width={42} />
                 </button>
                 <ul class="nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
