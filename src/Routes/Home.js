8fbec8791cfca84bfd4796db63b0897b8ecab91b
DESKTOP-P25C981\ian <iancobeaga96@gmail.com>
2023-08-25 11:42:52 -0300

    img home responsive

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div className='top-space text-center'>
-      <img alt="logo" src="https://i.ibb.co/BtJthmd/spider-logo.png" />
+    <div className='top-space text-center home-img'>
+      <img className='home-img' alt="logo" src="https://i.ibb.co/BtJthmd/spider-logo.png" />
     </div>
   );
 }
